682ba8b21ff3bd671b2dea626a17d8470adf3904
fmdkdd <fmdkdd@gmail.com>
2016-07-31 19:49:15 +0200

    Update ESLint to 3.2.0

--- addon/lint/javascript-lint.js
+++ addon/lint/javascript-lint.js
@@ -19,181 +19,100 @@
   };
 
   var config = {
-    "env": {
-      "browser": false,
-      "node": false,
-      "amd": false,
-      "mocha": false,
-      "jasmine": false
+    parserOptions: {
+      ecmaVersion: 6,
     },
 
-    "globals": {
-      "console": true
-    },
+    rules: {
+      // Errors
+      'no-dupe-args': 'error',
+      'no-dupe-keys': 'error',
+      'no-duplicate-case': 'error',
+      'no-irregular-whitespace': 'error',
+      'no-obj-calls': 'error',
 
-    "rules": {
-      "no-alert": 2,
-      "no-array-constructor": 2,
-      "no-bitwise": 0,
-      "no-caller": 2,
-      "no-catch-shadow": 2,
-      "no-comma-dangle": 0,
-      "no-cond-assign": [2, "except-parens"],
-      "no-console": 0,
-      "no-constant-condition": 2,
-      "no-control-regex": 2,
-      "no-debugger": 2,
-      "no-delete-var": 2,
-      "no-div-regex": 0,
-      "no-dupe-keys": 2,
-      "no-else-return": 0,
-      "no-empty": 2,
-      "no-empty-class": 2,
-      "no-empty-label": 2,
-      "no-eq-null": 0,
-      "no-eval": 2,
-      "no-ex-assign": 2,
-      "no-extend-native": 2,
-      "no-extra-bind": 2,
-      "no-extra-boolean-cast": 2,
-      "no-extra-parens": 1,
-      "no-extra-semi": 2,
-      "no-extra-strict": 2,
-      "no-fallthrough": 2,
-      "no-floating-decimal": 0,
-      "no-func-assign": 2,
-      "no-implied-eval": 2,
-      "no-inline-comments": 0,
-      "no-inner-declarations": [2, "functions"],
-      "no-invalid-regexp": 2,
-      "no-irregular-whitespace": 0,
-      "no-iterator": 2,
-      "no-label-var": 2,
-      "no-labels": 2,
-      "no-lone-blocks": 2,
-      "no-lonely-if": 0,
-      "no-loop-func": 2,
-      "no-mixed-requires": [0, false],
-      "no-mixed-spaces-and-tabs": [0, false],
-      "no-multi-spaces": 2,
-      "no-multi-str": 2,
-      "no-multiple-empty-lines": [0, {"max": 2}],
-      "no-native-reassign": 2,
-      "no-negated-in-lhs": 2,
-      "no-nested-ternary": 0,
-      "no-new": 2,
-      "no-new-func": 2,
-      "no-new-object": 2,
-      "no-new-require": 0,
-      "no-new-wrappers": 2,
-      "no-obj-calls": 2,
-      "no-octal": 2,
-      "no-octal-escape": 2,
-      "no-path-concat": 0,
-      "no-plusplus": 0,
-      "no-process-env": 0,
-      "no-process-exit": 2,
-      "no-proto": 2,
-      "no-redeclare": 2,
-      "no-regex-spaces": 2,
-      "no-reserved-keys": 0,
-      "no-restricted-modules": 0,
-      "no-return-assign": 2,
-      "no-script-url": 2,
-      "no-self-compare": 2,
-      "no-sequences": 2,
-      "no-shadow": 2,
-      "no-shadow-restricted-names": 2,
-      "no-space-before-semi": 1,
-      "no-spaced-func": 1,
-      "no-sparse-arrays": 2,
-      "no-sync": 0,
-      "no-ternary": 0,
-      "no-trailing-spaces": 0,
-      "no-undef": 1,
-      "no-undef-init": 2,
-      "no-undefined": 0,
-      "no-underscore-dangle": 2,
-      "no-unreachable": 2,
-      "no-unused-expressions": 0,
-      "no-unused-vars": 1,
-      "no-use-before-define": [1, "nofunc"],
-      "no-void": 0,
-      "no-warning-comments": [0, { "terms": ["todo", "fixme", "xxx"], "location": "start" }],
-      "no-with": 0,
-      "no-wrap-func": 2,
+      // Warnings
+      'no-ex-assign': 'warn',
+      'no-negated-in-lhs': 'warn',
+      'no-func-assign': 'warn',
+      'no-inner-declarations': 'warn',
+      'no-prototype-builtins': 'warn',
+      'no-sparse-arrays': 'warn',
+      'no-unreachable': 'warn',
+      'use-isnan': 'warn',
+      'valid-typeof': 'warn',
+      'array-callback-return': 'warn',
+      'eqeqeq': ['warn', 'smart'],
+      'no-redeclare': 'warn',
+      'no-self-assign': 'warn',
+      'no-self-compare': 'warn',
+      'no-useless-call': 'warn',
+      'no-restricted-globals': ['warn', 'self', '_$deadb33f'],
 
-      "block-scoped-var": 0,
-      "brace-style": [0, "1tbs"],
-      "camelcase": 0,
-      "comma-spacing": 0,
-      "comma-style": 0,
-      "complexity": [0, 11],
-      "consistent-return": 2,
-      "consistent-this": [0, "that"],
-      "curly": 0,
-      "default-case": 1,
-      "dot-notation": 2,
-      "eol-last": 0,
-      "eqeqeq": 2,
-      "func-names": 0,
-      "func-style": [0, "declaration"],
-      "global-strict": [2, "never"],
-      "guard-for-in": 0,
-      "handle-callback-err": 0,
-      "key-spacing": [2, { "beforeColon": false, "afterColon": true }],
-      "max-depth": [0, 4],
-      "max-len": [0, 80, 4],
-      "max-nested-callbacks": [0, 2],
-      "max-params": [0, 3],
-      "max-statements": [0, 10],
-      "new-cap": 2,
-      "new-parens": 2,
-      "one-var": 0,
-      "operator-assignment": [0, "always"],
-      "padded-blocks": 0,
-      "quote-props": 0,
-      "quotes": 0,
-      "radix": 1,
-      "semi": 0,
-      "sort-vars": 0,
-      "space-after-keywords": [1, "always"],
-      "space-before-blocks": [1, "always"],
-      "space-in-brackets": [0, "never"],
-      "space-in-parens": [0, "never"],
-      "space-infix-ops": 1,
-      "space-return-throw-case": 2,
-      "space-unary-ops": [2, { "words": true, "nonwords": false }],
-      "spaced-line-comment": [0, "always"],
-      "strict": 0,
-      "use-isnan": 2,
-      "valid-jsdoc": 0,
-      "valid-typeof": 2,
-      "vars-on-top": 0,
-      "wrap-iife": 0,
-      "wrap-regex": 0,
-      "yoda": [2, "never"]
+      // Style warnings
+      'no-extra-parens': 'warn',
+      'no-extra-semi': 'warn',
+      'no-unexpected-multiline': 'warn',
+      'block-scoped-var': 'warn',
+      'consistent-return': 'warn',
+      'curly': 'warn',
+      'default-case': 'warn',
+      'no-return-assign': 'warn',
+      'no-sequences': 'warn',
+      'no-unused-vars': 'warn',
+      'no-use-before-define': 'warn',
     }
   };
+
+  // Will affect a special 'style' severity category for these rules, with a
+  // distinct color in the editor.
+  var style_rules = [
+    'no-extra-parens',
+    'no-extra-semi',
+    'no-unexpected-multiline',
+    'block-scoped-var',
+    'consistent-return',
+    'curly',
+    'default-case',
+    'no-return-assign',
+    'no-sequences',
+    'no-unused-vars',
+    'no-use-before-define',
+  ]
 
   function validator(text, options) {
     if (!window.eslint) return [];
 
-    return eslint.verify(text, config).map(function(h) {
-      if (h.fatal) // Syntax error
+    return eslint.verify(text, config).map(function(lint) {
+      // Syntax error
+      if (lint.fatal) {
         return {
-          message: h.message,
-          severity: severity[h.severity],
-          from: CodeMirror.Pos(h.line - 1, h.column - 1),
-          to: CodeMirror.Pos(h.line - 1, h.column),
+          message: lint.message,
+          severity: severity[lint.severity],
+          from: CodeMirror.Pos(lint.line - 1, lint.column - 1),
+          to: CodeMirror.Pos(lint.line - 1, lint.column),
         };
+      }
+
+      // ESLint rule triggered
       else {
-        return {
-          message: h.message + ' (' + h.ruleId + ')',
-          severity: severity[h.severity],
-          from: CodeMirror.Pos(h.node.loc.start.line - 1, h.node.loc.start.column),
-          to: CodeMirror.Pos(h.node.loc.end.line - 1, h.node.loc.end.column),
+        var m = {
+          message: lint.message + ' (' + lint.ruleId + ')',
+          severity: severity[lint.severity],
+          from: CodeMirror.Pos(lint.line - 1, lint.column - 1),
         };
+
+        if (style_rules.indexOf(lint.ruleId) > -1) {
+          m.severity = 'style';
+        }
+
+        if (lint.endLine && lint.endColumn) {
+          m.to = CodeMirror.Pos(lint.endLine - 1, lint.endColumn - 1);
+        } else {
+          m.to = CodeMirror.Pos(lint.line - 1, lint.olumn);
+        }
+
+        return m;
       }
     });
   }
